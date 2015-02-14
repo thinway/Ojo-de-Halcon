a61f8b8f8eb5097a4c0f8c48ae51dc5abe018034
Fran Thinway <thinway@icloud.com>
2015-02-14 16:37:32 +0100

    Initial Commit

---
+++ OjoDeHalcon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OjoDeHalcon
+//
+//  Created by Fran Thinway on 14/2/15.
+//  Copyright (c) 2015 Thinway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
